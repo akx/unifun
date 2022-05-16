5cc85017f97e4672a6d7eb8c49e832793658c13d
Aarni Koskela <akx@iki.fi>
2022-05-16 14:29:47 +0300

    Add spacing option

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,15 @@ function transform(s: string, mapping: Map<number, number>) {
     .join("");
 }
 
-function computeResults(text: string) {
+function addSpacing(text: string, spacing: number) {
+  return Array.from(text).join(" ".repeat(spacing));
+}
+
+function computeResults(text: string, spacing: number) {
   const results: Record<string, string> = {};
-  const trim = text.trim();
+  const trim = addSpacing(text.trim(), spacing);
   if (trim.length) {
+    results["No Transform"] = trim;
     for (const name in qaz) {
       results[name] = transform(trim, qaz[name]);
     }
@@ -23,25 +28,37 @@ function computeResults(text: string) {
 
 function App() {
   const [text, setText] = React.useState("");
-  const results = React.useMemo(() => computeResults(text), [text]);
+  const [spacing, setSpacing] = React.useState(0);
+  const results = React.useMemo(() => computeResults(text, spacing), [text, spacing]);
   return (
     <main>
-      <input
-        type="text"
-        placeholder="Enter text here..."
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-      />
+      <header>
+        <input
+          type="text"
+          placeholder="Enter text here..."
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <label>
+          <span>Spaces between letters</span>
+          <input
+            type="number"
+            min={0}
+            value={spacing}
+            onChange={(e) => setSpacing(Number(e.target.value))}
+          />
+        </label>
+      </header>
       <table>
         <tbody>
-          {Object.entries(results).map(([name, text]) => (
-            <tr key={name}>
-              <th scope="row">{name}</th>
-              <td>
-                <input type="text" readOnly value={text} />
-              </td>
-            </tr>
-          ))}
+        {Object.entries(results).map(([name, text]) => (
+          <tr key={name}>
+            <th scope="row">{name}</th>
+            <td>
+              <input type="text" readOnly value={text} />
+            </td>
+          </tr>
+        ))}
         </tbody>
       </table>
       <footer>
