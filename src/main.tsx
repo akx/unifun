873287f834548c47d870d627468bf3e84afd1779
Aarni Koskela <akx@iki.fi>
2024-04-22 19:45:18 +0300

    Upgrade bits and bobs (#8)

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { createRoot } from "react-dom/client";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root"),
-);
+const rootEl = document.getElementById("root");
+if (rootEl) {
+  createRoot(rootEl).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
+}
